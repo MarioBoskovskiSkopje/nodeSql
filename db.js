316add6cc74396a0550377c0e46eb681a3524b59
MarioBoskovskiSkopje <marioboskovski123@gmail.com>
2019-02-27 13:49:40 +0000

    commit

--- db.js
+++ db.js
@@ -21,14 +21,16 @@ connection.on("connect", function(err) {
 
 function executeStatement(partNo, res) {
   request = new Request(
-    `SELECT inmast.fpartno, inmast.frev, inmast.fcstscode, inmast.fdescript, inmast.fsource, inmast.fstdcost, inmast.fprice
-      FROM M2MDATA01.dbo.inmast inmast
+    `SELECT inmast.fpartno,inmast.frev, inmast.fcstscode, inmast.fdescript, inmast.fsource, inmast.fstdcost, inmast.fprice,invcur.FLANYCUR,invcur.fcpartrev
+      FROM M2MDATA01.dbo.inmast inmast FULL JOIN M2MDATA01.dbo.invcur ON (invcur.fcpartno = inmast.fpartno)
       WHERE (inmast.fpartno LIKE '${partNo}' AND inmast.fcstscode LIKE 'a')
       ORDER BY inmast.fpartno
       `,
     function(err) {
       if (err) {
+
         console.log(err);
+          return  res.send({status:1,partNum:partNo})
       }
     }
   );
@@ -39,6 +41,7 @@ function executeStatement(partNo, res) {
     columns.forEach(function(column) {
       if (column.value === null) {
         console.log("NULL");
+return res.send({status:1,partNum:partNo});
       } else {
         console.log(column.metadata.colName);
         result.push({ [column.metadata.colName]: column.value });
@@ -49,45 +52,61 @@ function executeStatement(partNo, res) {
     console.log(result);
     //    return res.send({ result });
     result = [];
+console.log("RESULT ROW");
+return res.send({status:1,partNum:partNo});
+
   });
 
   request.on("done", function(rowCount, more) {
     console.log(rowCount + " rows returned");
   });
   request.on("requestCompleted", function(rowCount, more) {
-    console.log("requestCompleted");
+    console.log("requestCompleted",data);
+  // if(data.length > 0){
     res.send({ data });
+//}else{
+//res.send({status:1,partNum:partNo});
+//}
   });
+try{
   connection.execSql(request);
+}catch(error){
+return res.send({status:1,partNum:partNo});
+}
 }
 
-function getPartPromise(partNo) {
+function getPartPromise(partNo,res) {
   return new Promise((resolve, reject) => {
     console.log("started promise");
     request = new Request(
-      `SELECT inmast.fpartno, inmast.frev, inmast.fcstscode, inmast.fdescript, inmast.fsource, inmast.fstdcost, inmast.fprice
-              FROM M2MDATA01.dbo.inmast inmast
+      `SELECT inmast.fpartno, inmast.frev, inmast.fcstscode, inmast.fdescript, inmast.fsource, inmast.fstdcost, inmast.fprice,invcur.FLANYCUR,invcur.fcpartrev
+              FROM M2MDATA01.dbo.inmast inmast FULL JOIN M2MDATA01.dbo.invcur ON (invcur.fcpartno = inmast.fpartno)
               WHERE (inmast.fpartno LIKE '${partNo}' AND inmast.fcstscode LIKE 'a')
               ORDER BY inmast.fpartno
               `,
       function(err) {
         if (err) {
+             
           console.log(err);
-          return reject(err);
+          return {status:1,partNum:partNo};
         }
       }
     );
     let result = {};
     let data = [];
+//console.log("before row");
     request.on("row", function(columns) {
-      //      console.log("columns",columns);
+           // console.log("columns",columns);
       columns.forEach(function(column) {
         if (column.value === null) {
           console.log("NULL");
+             
         } else {
           let value = column.value ? column.value.toString().trim() : "";
           result[[column.metadata.colName]] = value;
+              
         }
+//console.log("inside row");
       });
       result["partNum"] = partNo;
       data.push(result);
@@ -98,11 +117,16 @@ function getPartPromise(partNo) {
       console.log(rowCount + " rows returned");
     });
     request.on("requestCompleted", function(rowCount, more) {
-      console.log("requestCompleted");
+      console.log("requestCompleted",data);
 
       return resolve(data);
+
     });
+ 	try{
     connection.execSql(request);
+}catch(error){
+return [];
+}
   });
 }
 
@@ -154,3 +178,4 @@ function getPartPromise(partNo) {
 // }
 
 module.exports = { executeStatement, getPartPromise };
+
