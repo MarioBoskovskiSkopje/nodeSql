8defddac45a6ab3ba2ff3d4249902439bbcd6156
Rohed <oliveraluloski@gmail.com>
2019-07-09 09:44:17 +0200

    set mssql

---
+++ db_mssql.js
@@ -0,0 +1,178 @@
+var sql = require('mssql');
+const config = {
+  user: 'SymPro',
+  password: 'k3d6ATTO8Loa',
+  server: '69.63.100.74',
+  database: 'M2MDATA01',
+  // If you are on Microsoft Azure, you need this:
+  //options: {encrypt: true, database: 'AdventureWorks'}
+};
+
+(async function() {
+  try {
+    let pool = await sql.connect(config);
+    let result1 = await pool.request();
+    console.log('connected');
+  } catch (err) {
+    console.log('request err', err);
+    // ... error checks
+  }
+})();
+
+sql.on('error', err => {
+  console.log('sql err', err);
+  // ... error handler
+});
+function executeStatement(partNo, res) {
+  request = new Request(
+    `SELECT inmast.fpartno,inmast.frev, inmast.fcstscode, inmast.fdescript, inmast.fsource, inmast.fstdcost, inmast.fprice,invcur.FLANYCUR,invcur.fcpartrev
+      FROM M2MDATA01.dbo.inmast inmast FULL JOIN M2MDATA01.dbo.invcur ON (invcur.fcpartno = inmast.fpartno)
+      WHERE (inmast.fpartno LIKE '${partNo}' AND inmast.fcstscode LIKE 'a')
+      ORDER BY inmast.fpartno
+      `,
+    function(err) {
+      if (err) {
+        console.log(err);
+        return res.send({ status: 1, partNum: partNo });
+      }
+    },
+  );
+  let result = [];
+  let data = [];
+  request.on('row', function(columns) {
+    //      console.log("columns",columns);
+    columns.forEach(function(column) {
+      if (column.value === null) {
+        console.log('NULL');
+        return res.send({ status: 1, partNum: partNo });
+      } else {
+        console.log(column.metadata.colName);
+        result.push({ [column.metadata.colName]: column.value });
+        // result += column.value + " ";
+      }
+    });
+    data.push(result);
+    console.log(result);
+    //    return res.send({ result });
+    result = [];
+    console.log('RESULT ROW');
+    return res.send({ status: 1, partNum: partNo });
+  });
+
+  request.on('done', function(rowCount, more) {
+    console.log(rowCount + ' rows returned');
+  });
+  request.on('requestCompleted', function(rowCount, more) {
+    console.log('requestCompleted', data);
+    // if(data.length > 0){
+    res.send({ data });
+    //}else{
+    //res.send({status:1,partNum:partNo});
+    //}
+  });
+  try {
+    connection.execSql(request);
+  } catch (error) {
+    return res.send({ status: 1, partNum: partNo });
+  }
+}
+
+function getPartPromise(partNo, res) {
+  return new Promise((resolve, reject) => {
+    console.log('started promise');
+    request = new Request(
+      `SELECT inmast.fpartno, inmast.frev, inmast.fcstscode, inmast.fdescript, inmast.fsource, inmast.fstdcost, inmast.fprice,invcur.FLANYCUR,invcur.fcpartrev
+              FROM M2MDATA01.dbo.inmast inmast FULL JOIN M2MDATA01.dbo.invcur ON (invcur.fcpartno = inmast.fpartno)
+              WHERE (inmast.fpartno LIKE '${partNo}' AND inmast.fcstscode LIKE 'a')
+              ORDER BY inmast.fpartno
+              `,
+      function(err) {
+        if (err) {
+          console.log('partPromiseErr', err);
+          return { status: 1, partNum: partNo };
+        }
+      },
+    );
+    let result = {};
+    let data = [];
+    //console.log("before row");
+    request.on('row', function(columns) {
+      // console.log("columns",columns);
+      columns.forEach(function(column) {
+        if (column.value === null) {
+          console.log('NULL');
+        } else {
+          let value = column.value ? column.value.toString().trim() : '';
+          result[[column.metadata.colName]] = value;
+        }
+        //console.log("inside row");
+      });
+      result['partNum'] = partNo;
+      data.push(result);
+      result = {};
+    });
+
+    request.on('done', function(rowCount, more) {
+      console.log(rowCount + ' rows returned');
+    });
+    request.on('requestCompleted', function(rowCount, more) {
+      console.log('requestCompleted', data);
+
+      return resolve(data);
+    });
+    try {
+      connection.execSql(request);
+    } catch (error) {
+      return [];
+    }
+  });
+}
+
+// function getPartInfo(partNum) {
+//   return new Promise((resolve, reject) => {
+//     console.log("started promise");
+//     request = new Request(
+//       `SELECT jomast.fsono, jomast.fjobno, jomast.fstatus, jomast.fpartno, joitem.fmqty, jodbom.fbompart, jodbom.ftotqty, inmast.flocate1, inmast.fmatlcost
+//           FROM M2MDATA01.dbo.inmast inmast, M2MDATA01.dbo.jodbom jodbom, M2MDATA01.dbo.joitem joitem, M2MDATA01.dbo.jomast jomast
+//           WHERE jodbom.fjobno = joitem.fjobno AND joitem.fjobno = jomast.fjobno AND inmast.fpartno = jodbom.fbompart AND ((jomast.fstatus='open') AND (jomast.fcompany Like '${partNum}') OR (jomast.fstatus='released') AND (jomast.fcompany Like '${partNum}') OR (jomast.fstatus='completed') AND (jomast.fcompany Like '${partNum}'))
+//           ORDER BY jomast.fpartno`,
+//       function(err) {
+//         if (err) {
+//           console.log(err);
+//           return reject(err);
+//         }
+//       }
+//     );
+//     let result = {};
+//     let data = [];
+//     request.on("row", function(columns) {
+//       //      console.log("columns",columns);
+//       columns.forEach(function(column) {
+//         if (column.value === null) {
+//           console.log("NULL");
+//         } else {
+//           //console.log(column.metadata.colName);
+//           //   result.push({ [column.metadata.colName]: column.value });
+//           result[[column.metadata.colName]] = column.value;
+//           // result += column.value + " ";
+//         }
+//       });
+//       data.push(result);
+//       // console.log(result);
+//       //    return res.send({ result });
+//       result = {};
+//     });
+
+//     request.on("done", function(rowCount, more) {
+//       console.log(rowCount + " rows returned");
+//     });
+//     request.on("requestCompleted", function(rowCount, more) {
+//       console.log("requestCompleted");
+//       return resolve(data);
+//       //res.send({ data });
+//     });
+//     connection.execSql(request);
+//   });
+// }
+
+module.exports = { executeStatement, getPartPromise };
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const db = require("./db");
+const bodyParser = require('body-parser');
+const db = require('./db_mssql');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/test", (req, res) => {
+app.get('/test', (req, res) => {
   res.send({ result: 0 });
 });
 
-app.get("/validate", (req, res) => {
+app.get('/validate', (req, res) => {
   const { partNo } = req.query;
   if (partNo) {
     return db.executeStatement(partNo, res);
@@ -19,19 +19,19 @@ app.get("/validate", (req, res) => {
   //  res.send(req.query);
 });
 
-app.post("/validateparts", async (req, res) => {
-const {typeCodes = ["6X"]}=req.body
+app.post('/validateparts', async (req, res) => {
+  const { typeCodes = ['6X'] } = req.body;
 
-  const {idArr=[] }=req.body;
-if(typeCodes.length>2){
-for(let i = 0;i<typeCodes.length;i++){
-const element = typeCodes[i];
-idArr.push(element);
-}
-}
-let filteredArray = idArr.filter((item,pos) => {
-return idArr.indexOf(item) == pos;
-});
+  const { idArr = [] } = req.body;
+  if (typeCodes.length > 2) {
+    for (let i = 0; i < typeCodes.length; i++) {
+      const element = typeCodes[i];
+      idArr.push(element);
+    }
+  }
+  let filteredArray = idArr.filter((item, pos) => {
+    return idArr.indexOf(item) == pos;
+  });
   const { partNumbers } = req.body;
   console.log(req.body);
   //let parsedPartNumbers = JSON.parse(partNumbers);
@@ -42,47 +42,43 @@ return idArr.indexOf(item) == pos;
     for (let i = 0; i < partNumbers.length; i++) {
       if (!truthTable[partNumbers[i]]) {
         let prefillNumber = `${filteredArray[j]}${partNumbers[i]}`;
-        console.log("Num part", prefillNumber);
+        console.log('Num part', prefillNumber);
         let resultData = await db.getPartPromise(prefillNumber);
-        console.log("REs", resultData);
+        console.log('REs', resultData);
         if (resultData.length > 0) {
           truthTable[partNumbers[i]] = true;
           result.push(resultData);
         }
       } else {
-
         continue;
       }
     }
   }
 
   res.send({ result });
-
 });
 
-app.post("/partinfo", async (req, res) => {
+app.post('/partinfo', async (req, res) => {
   const { partNumbers } = req.body;
   console.log(req.body);
   //let parsedPartNumbers = JSON.parse(partNumbers);
 
   let result = [];
-let errors = [];
+  let errors = [];
   for (let i = 0; i < partNumbers.length; i++) {
     let resultData = await db.getPartPromise(partNumbers[i]);
-    console.log("RESULTDATA", resultData);
-   // console.log(partNumbers[i]);
-if(resultData.length > 0){
-    result.push(resultData);
-}else{
-    errors.push({status:1,partNum:partNumbers[i]});
-}
+    console.log('RESULTDATA', resultData);
+    // console.log(partNumbers[i]);
+    if (resultData.length > 0) {
+      result.push(resultData);
+    } else {
+      errors.push({ status: 1, partNum: partNumbers[i] });
+    }
   }
 
-  res.send({ result,errors })
-
+  res.send({ result, errors });
 });
 
 const port = process.env.PORT || 80;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
