b8fb054f39c70beadeb5a93e34cd5d1defa7cc80
Rohed <oliveraluloski@gmail.com>
2019-07-09 09:51:41 +0200

    set mssql

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const db = require('./db_mssql');
+const db = require('./db');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
